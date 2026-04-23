89c76cac1205a5c587c48b0bd2d98899d66d4b9c
kimjaebeom07 <lovejb0831@gmail.com>
2026-04-24 02:02:59 +0900

    Add files via upload

---
+++ 0417/13.py
@@ -0,0 +1,7 @@
+new_list = []
+for x in range(1, 30):
+            for y in range(x, 30):
+                        for z in range(y, 30):
+                            if x**2+y**2==z**2:
+                                new_list.append((x, y, z))
+print(new_list)
